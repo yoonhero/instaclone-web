c4e3b0572bbc92c8b375e4e0af198e26bc392984
yoonhero <yoonhero06@naver.com>
2021-03-09 08:26:02 +0900

    3/9 finished

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -1,4 +1,4 @@
-import { gql, useMutation, useQuery } from "@apollo/client";
+import { gql, useApolloClient, useMutation, useQuery } from "@apollo/client";
 import { useLocation, useParams } from "react-router";
 import { PHOTO_FRAGMENT } from "../fragments";
 import styled from "styled-components";
@@ -134,33 +134,66 @@ const ProfileBtn = styled(Button).attrs({
 const Profile = () => {
   const { username } = useParams();
   const { data: userData } = useUser();
+  const client = useApolloClient();
   const { data, loading } = useQuery(SEE_PROFILE_QUERY, {
     variables: {
       username,
     },
   });
 
+  const unfollowUserUpdate = (cache, result) => {
+    const {
+      data: {
+        unfollowUser: { ok },
+      },
+    } = result;
+    if (!ok) {
+      return;
+    }
+    cache.modify({
+      id: `User:${username}`,
+      fields: {
+        isFollowing(prev) {
+          return false;
+        },
+        totalFollowers(prev) {
+          return prev - 1;
+        },
+      },
+    });
+  };
+
   const [unfollowUser] = useMutation(UNFOLLOW_USER_MUTATION, {
     variables: {
       username,
     },
-    refetchQueries: [
-      { query: SEE_PROFILE_QUERY, variables: { username } },
-      {
-        query: ME_QUERY,
-      },
-    ],
+    update: unfollowUserUpdate,
   });
+  const followUserCompleted = (data) => {
+    const {
+      followUser: { ok },
+    } = data;
+    if (!ok) {
+      return;
+    }
+    const { cache } = client;
+    cache.modify({
+      id: `User:${username}`,
+      fields: {
+        isFollowing(prev) {
+          return true;
+        },
+        totalFollowers(prev) {
+          return prev + 1;
+        },
+      },
+    });
+  };
   const [followUser] = useMutation(FOLLOW_USER_MUTATION, {
     variables: {
       username,
     },
-    refetchQueries: [
-      { query: SEE_PROFILE_QUERY, variables: { username } },
-      {
-        query: ME_QUERY,
-      },
-    ],
+    onCompleted: followUserCompleted,
   });
 
   const getButton = (seeProfile) => {
